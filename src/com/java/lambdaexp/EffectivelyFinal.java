894631fa8d924991be4735b4a178a3759568b243
Hari Krishna <harikrishnabhuvaneshwari@yahoo.com>
2023-06-07 09:07:52 +0530

    docs change

--- src/com/java/lambdaexp/EffectivelyFinal.java
+++ src/com/java/lambdaexp/EffectivelyFinal.java
@@ -6,7 +6,7 @@ why the surrounding scope code variable(or) local variable, when used inside an
 anonymous inner class(or)lambda expression must be declared final or effectively final?
 
 
-Note : Leaving this class in compilation error state to understand.
+Note : Leaving this class in compilation error state for better understanding.
 */
 
 public class EffectivelyFinal {
@@ -25,6 +25,16 @@ public class EffectivelyFinal {
         This will ensure that the variables are always monitored and leads to improved consistency, reliability,
         reduced error code etc.
 
+        Closure Vs. Lambda
+        Java lambda expression is a way to define a function inline rather than the standard method of declaration.
+        It provides a concise way to represent a function.
+        While closure is a function that encompasses its surrounding state by referencing fields that are external to its body.
+        Note that both are functions but not all closures are lambdas and not all lambdas are closures.
+
+        The difference between closure and lambda is quite equivalent to the distinction between a class and an instance of the class.
+        A class exists only in source code. it doesn't exist at runtime. What exists at runtime are objects of the class type.
+        Closures are to lambdas as objects are to classes.
+
         That's why java compiler is asking you to make that variable explicitly final (or) if you're not making it
         then I(compiler) will consider it as effectively final and will keep monitoring you for the changes.
 
@@ -41,6 +51,10 @@ public class EffectivelyFinal {
         Printer anonymousInnerClass = new Printer() {
             @Override
             public void print(int i) {
+                /*
+                 compilation error, because we changed the value of a after initializing.
+                 Note : compiler monitors(closure) you.
+                 */
                 System.out.println(i + a);
             }
         };
